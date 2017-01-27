709c44a240b54a835c29a2e82ff0ac2152d7523e
Anders Sjögren <as@kth.se>
2017-01-27 13:38:03 +0100

    Uppdaterat script-flier med MY_PRJ_PATH

--- Build-script/Setup-mingw32-AS.bat
+++ Build-script/Setup-mingw32-AS.bat
@@ -4,6 +4,7 @@ REM GCC_EXEC_PREFIX is optional, but having it here doesn't hurt.
 REM
 REM - Compiler settings on this computer -
 PATH=%PATH%;C:\TPFAPPS\egcs\bin\egcs\bin
+PATH=%PATH%;C:\TPFAPPS\egcs\lib\gcc-lib\i386-mingw32\egcs-2.91.66
 SET GCC_EXEC_PREFIX=c:\TPFAPPS\egcs\lib\gcc-lib\
 SET CPATH=C:\TPFAPPS\egcs\i386-mingw32\include\
 SET LIBRARY_PATH=C:\TPFAPPS\egcs\i386-mingw32\lib\
@@ -11,10 +12,11 @@ REM
 REM ------- Project settings and setup --------
 REM --- Move to working directory (compiled output )---
 H:
-cd \as\Projekt\GitProj\Ranta\Git-filer\Ranta\Build-script\
+cd \as\Projekt\RantaPaRanta\Ranta\Build-script\
 REM
 REM --- Set the common basic path which is equal for all C-code subdiretories ---
-SET MY_C_PATH=H:\as\Projekt\GitProj\Ranta\Git-filer\Ranta\Source-Code\
+SET MY_C_PATH=H:\as\Projekt\RantaPaRanta\Ranta\Source-Code\
+SET MY_PRJ_PATH=H:\as\Projekt\RantaPaRanta\
 REM --- Set CPATH to all necessary source code directorys ---
 SET CPATH=%CPATH%;%MY_C_PATH%Include-common\;%MY_C_PATH%Module-Main\Include-main\;%MY_C_PATH%Module-Tabell\Include-tabell\;%MY_C_PATH%Module-Inmatning\Include-inmatning\;%MY_C_PATH%Module-Kalkylator\Include-kalkylator\
 
--- Build-script/bygg-proj-AS.bat
+++ Build-script/bygg-proj-AS.bat
@@ -1,9 +1,9 @@
 REM Kompilering till objektfiler
-gcc -c ..\Source-Code\Module-Main\main.c -o ..\..\..\Compiled-output\main.o
-gcc -c ..\Source-Code\Module-Inmatning\inmatnin.c -o ..\..\..\Compiled-output\inmatnin.o
-gcc -c ..\Source-Code\Module-Tabell\tabell.c -o ..\..\..\Compiled-output\tabell.o
-gcc -c ..\Source-Code\Module-Kalkylator\kalkylat.c -o ..\..\..\Compiled-output\kalkylat.o
+gcc -c %MY_C_PATH%Module-Main\main.c -o %MY_PRJ_PATH%Compiled-output\main.o
+gcc -c %MY_C_PATH%Module-Inmatning\inmatnin.c -o %MY_PRJ_PATH%Compiled-output\inmatnin.o
+gcc -c %MY_C_PATH%Module-Tabell\tabell.c -o %MY_PRJ_PATH%Compiled-output\tabell.o
+gcc -c %MY_C_PATH%Module-Kalkylator\kalkylat.c -o %MY_PRJ_PATH%Compiled-output\kalkylat.o
 REM
 REM Linking
-gcc -o ..\..\..\Compiled-output\ranta.exe ..\..\..\Compiled-output\main.o ..\..\..\Compiled-output\inmatnin.o ..\..\..\Compiled-output\tabell.o ..\..\..\Compiled-output\kalkylat.o   
+gcc -o %MY_PRJ_PATH%Compiled-output\ranta.exe 	%MY_PRJ_PATH%Compiled-output\main.o  	%MY_PRJ_PATH%Compiled-output\inmatnin.o 	%MY_PRJ_PATH%Compiled-output\tabell.o 	%MY_PRJ_PATH%Compiled-output\kalkylat.o   
 
